22e690526e740cdbda3393001169e0e36ba1215e
Hassaan Jalal <hassyku786@gmail.com>
2025-09-14 21:29:04 +0000

    npm install firebase
    // Import the functions you need from the SDKs you

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "nextn-65825.firebaseapp.com",
-  projectId: "nextn-65825",
-  storageBucket: "nextn-65825.appspot.com",
-  messagingSenderId: "1069553184333",
-  appId: "1:1069553184333:web:0b7d34e97669d2a6a68f04",
-  measurementId: "G-L9CSL22J5W"
+  apiKey: "AIzaSyApRM4MIHiZCRgoLxGkRI-6nnlmvAO_9CA",
+  authDomain: "chirpchat-yi7xn.firebaseapp.com",
+  projectId: "chirpchat-yi7xn",
+  storageBucket: "chirpchat-yi7xn.appspot.com",
+  messagingSenderId: "786711867654",
+  appId: "1:786711867654:web:a845ace12b0f9c526c2e87"
 };
 
 // Initialize Firebase
