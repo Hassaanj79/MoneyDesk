58a184cedd47fd7a62d9261a7da1aec45701b5bb
Hassaan Jalal <hassyku786@gmail.com>
2025-09-14 22:16:14 +0000

    when I am clicking on dashboard why it is not goig there ?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 
-
 "use client";
 
-import { redirect } from 'next/navigation';
+import DashboardGrid from "@/components/dashboard/dashboard-grid";
 
-export default function RootPage() {
-  redirect('/transactions');
+export default function DashboardPage() {
+  return <DashboardGrid />;
 }
