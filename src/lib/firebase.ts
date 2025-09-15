e2ec5e53be4b967c18a7a5d54f01454b66a3ab02
Hassaan Jalal <hassyku786@gmail.com>
2025-09-15 04:14:36 +0000

    why continue with google is not working ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApRM4MIHiZCRgoLxGkRI-6nnlmvAO_9CA",
-  authDomain: "chirpchat-yi7xn.firebaseapp.com",
-  projectId: "chirpchat-yi7xn",
-  storageBucket: "chirpchat-yi7xn.appspot.com",
-  messagingSenderId: "786711867654",
-  appId: "1:786711867654:web:a845ace12b0f9c526c2e87"
+  apiKey: "AIzaSyA5iF4n-xI1-g6p3aWfS_x5J8Q9y7Rz-wE",
+  authDomain: "moneydesk-8f431.firebaseapp.com",
+  projectId: "moneydesk-8f431",
+  storageBucket: "moneydesk-8f431.appspot.com",
+  messagingSenderId: "1095204439126",
+  appId: "1:1095204439126:web:158e92f4f205a25e16b940"
 };
 
 // Initialize Firebase
